ec45e0ea125f47066a399845009a205c959555c7
Wuraim <lorgnierg@yahoo.com>
2024-12-03 21:40:56 +0100

    day 3

---
+++ 2024/day3/day3/day3/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+string input = File.ReadAllText("input.txt");
+
+string pattern = @"mul\([0-9]{1,3}\,[0-9]{1,3}\)";
+var matches = Regex.Matches(input, pattern);
+
+int finalValue = 0;
+foreach (Match match in matches)
+{
+    string stringMatch = match.ToString();
+    var values = Regex.Matches(stringMatch, @"[0-9]{1,3}");
+    int a = int.Parse(values[0].ToString());
+    int b = int.Parse(values[1].ToString());
+
+    finalValue += a * b;
+}
+
+Console.WriteLine("finalValue part 1: " + finalValue.ToString());
+
+var matchesPart2 = Regex.Matches(input, @"mul\([0-9]{1,3}\,[0-9]{1,3}\)|do\(\)|don\'t\(\)");
+bool isEnabled = true;
+int finalValue2 = 0;
+foreach (Match match in matchesPart2)
+{
+    string stringMatch = match.ToString();
+    
+    bool isDo = Regex.Match(match.ToString(), @"do\(\)").Success;
+    bool isDont = Regex.Match(match.ToString(), @"don\'t\(\)").Success;
+    bool isMul = Regex.Match(match.ToString(), @"mul\([0-9]{1,3}\,[0-9]{1,3}\)").Success;
+
+    isEnabled = isMul ? isEnabled : isDo;
+
+    if (isEnabled && isMul)
+    {
+        var values = Regex.Matches(stringMatch, @"[0-9]{1,3}");
+        int a = int.Parse(values[0].ToString());
+        int b = int.Parse(values[1].ToString());
+
+        // Console.WriteLine(a.ToString() + ' ' + b.ToString());
+        finalValue2 += a * b;
+    }
+}
+
+Console.WriteLine("finalValue2 " + finalValue2.ToString());
+Console.ReadKey();
--- 2024/day3/day3/day3/obj/Debug/net8.0/day3.AssemblyInfo.cs
+++ 2024/day3/day3/day3/obj/Debug/net8.0/day3.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("day1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("day3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf76e4c4163758b60dceae854ff4f42c182861a9")]
-[assembly: System.Reflection.AssemblyProductAttribute("day1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("day1")]
+[assembly: System.Reflection.AssemblyProductAttribute("day3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("day3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
